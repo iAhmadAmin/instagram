4cb6409ac9cefd0b2fb98bb6f0e61bf118a84219
Ahmad Amin <ammyhunn@gmail.com>
2021-01-28 22:01:50 +0500

    added login & signup pages

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: SplashPage(),
     );
   }
 }
