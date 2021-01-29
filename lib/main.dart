6e9af706133a18e0e172caa0e4fd85f66718fe8b
Ahmad Amin <ammyhunn@gmail.com>
2021-01-29 14:59:52 +0500

    new branch for backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:instagram/ui/pages/Auth/login_page.dart';
 import 'package:instagram/ui/pages/Splash/splash_page.dart';
 import 'package:get/get.dart';
 
